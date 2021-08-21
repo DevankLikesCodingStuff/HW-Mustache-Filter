b65419b2a6fc9ba8352efc6904cc01fb92a5013d
DevankLikesCodingStuff <77108500+DevankLikesCodingStuff@users.noreply.github.com>
2021-08-21 15:58:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+noseX=0;
+noseY=0;
 
+function preload() {
+    mustach = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup()
@@ -24,13 +27,16 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y)
+        noseX = results[0].pose.nose.x - 18;
+        noseY = results[0].pose.nose.y - 15;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
     }
 }
 
 function draw(){
     image(video,0,0,300,300)
+    image(mustach,noseX,noseY,50,50);
 }
 
 function take_snapshot()
